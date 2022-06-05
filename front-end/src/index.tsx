b108333e8183718d75073cf8c7933a46c7958e5c
Jingheng "Jimmy" Wang <jinghenw@andrew.cmu.edu>
2022-06-05 13:45:50 -0700

    Integrate React Router (JW)

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App />} />
+            
+        </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
