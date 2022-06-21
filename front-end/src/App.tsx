14485e19e7a37bc724bc06a2e2868126f6da3d62
Jingheng "Jimmy" Wang <jinghenw@andrew.cmu.edu>
2022-06-21 12:10:24 -0700

    Solve Merge Conflicts (JW)

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,15 +7,20 @@ import AuthRedirect from "./routes/auth/AuthRedirect";
 import AuthCallback from "./routes/auth/AuthCallback";
 import Index from "./routes/Index";
 import {GlobalStateProvider} from "./components/GlobalStateProvider";
+import ExamQuestions from "./routes/course/ExamQuestions";
+import ExamInstructions from "./routes/course/ExamInstructions";
 
 const App = () => {
     return (
         <div className="App">
             <GlobalStateProvider>
                 <Routes>
-                    <Route path='/' element={<Index/>}/>
+                    <Route path='/' element={<Index />}/>
                     <Route path="dashboard" element={<Dashboard/>}/>
                     <Route path="assessments" element={<Assessments/>}/>
+                    <Route path="assessments/:exam_id" element={<ExamInstructions/>}/>
+                    <Route path="assessments/:exam_id/questions" element={<ExamQuestions/>}/>
+                    <Route path="questions" element={<ExamQuestions/>}/>
                     <Route path="oauth" element={<AuthRedirect/>}/>
                     <Route path="oauth-callback" element={<AuthCallback/>}/>
                 </Routes>
