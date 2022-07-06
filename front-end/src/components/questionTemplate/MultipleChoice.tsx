9b0191a732810911495908391ca94686c8557dc1
Yujia-Wan <yujiawan@andrew.cmu.edu>
2022-07-06 18:01:13 -0400

    Add storage of choice subquestion (YW)

--- front-end/src/components/questionTemplate/MultipleChoice.tsx
+++ front-end/src/components/questionTemplate/MultipleChoice.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 import QuestionLayout from "./QuestionLayout";
-import { subQuestionDataType } from "./subQuestionDataType";
+import { choiceDataType, subQuestionDataType } from "./subQuestionDataType";
+import usePersistState from "../../utils/usePersistState";
+
+const OneInMultipleChoice = ({choice, name, storageKey} : {choice: choiceDataType, name: string, storageKey: string}) => {
+    const {value, setValue} = usePersistState("", storageKey);
+    return (
+        <Form.Check type='checkbox'
+            name={name}
+            key={storageKey}
+            id={storageKey}
+            label={choice.content}
+            value={value}
+            defaultChecked={false}
+            onChange={(event) => {
+                const newValue = choice.choiceId;
+                event.target.checked ? setValue(newValue) : setValue("");
+            }} />
+    )
+}
 
 const MultipleChoice = ({data, headerId} : {data: subQuestionDataType, headerId: string}) => {
-    const checkboxes = data.choices.map((choice: any) => (
-        <Form.Check type='checkbox'
-            name={`Q${headerId}_sub${data.questionId}`}
-            key={`Q${headerId}_sub${data.questionId}_choice${choice.choiceId}`}
-            id={`Q${headerId}_sub${data.questionId}_choice${choice.choiceId}`}
-            label={choice.content} />
-    ));
+    const checkboxes = data.choices.map((choice: any) => {
+        let storageKey = `Q${headerId}_sub${data.questionId}_choice${choice.choiceId}`;
+        return (
+            <OneInMultipleChoice
+                choice={choice}
+                name={`Q${headerId}_sub${data.questionId}`}
+                key={storageKey}
+                storageKey={storageKey} />
+        )
+    });
 
     return (
         <QuestionLayout questionId={data.questionId.toString()} description={data.description}>
--- front-end/src/components/questionTemplate/SingleChoice.tsx
+++ front-end/src/components/questionTemplate/SingleChoice.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
 import { Form } from 'react-bootstrap';
 import QuestionLayout from "./QuestionLayout";
-import { subQuestionDataType } from "./subQuestionDataType";
+import { choiceDataType, subQuestionDataType } from "./subQuestionDataType";
+import usePersistState from "../../utils/usePersistState";
+
+const OneInSingleChoice = ({choice, name, storageKey} : {choice: choiceDataType, name: string, storageKey: string}) => {
+    const {value, setValue} = usePersistState("", storageKey);
+    return (
+        <Form.Check type='radio'
+            name={name}
+            key={storageKey}
+            id={storageKey}
+            label={choice.content}
+            value = {value}
+            defaultChecked={false}
+            onChange={(event) => {
+                const newValue = choice.choiceId;
+                event.target.checked ? setValue(newValue) : setValue("");
+            }} />
+    )
+}
 
 const SingleChoice = ({data, headerId} : {data: subQuestionDataType, headerId: string}) => {
-    const radios = data.choices.map((choice: any) => (
-        <Form.Check type='radio'
-            name={`Q${headerId}_sub${data.questionId}`}
-            key={`Q${headerId}_sub${data.questionId}_choice${choice.choiceId}`}
-            id={`Q${headerId}_sub${data.questionId}_choice${choice.choiceId}`}
-            label={choice.content} />
-    ));
+    const radios = data.choices.map((choice) => {
+        let storageKey = `Q${headerId}_sub${data.questionId}_choice${choice.choiceId}`;
+        return (
+            <OneInSingleChoice
+                choice={choice}
+                name={`Q${headerId}_sub${data.questionId}`}
+                key={storageKey}
+                storageKey={storageKey} />
+        )
+    });
 
     return (
         <QuestionLayout questionId={data.questionId.toString()} description={data.description}>
