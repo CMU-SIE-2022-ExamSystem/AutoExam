7903d42a6321e94fcc31982de5dd64af073526ce
Yujia-Wan <yujiawan@andrew.cmu.edu>
2022-07-18 19:56:45 -0400

    Add Tag of Question Bank (YW)

--- front-end/src/routes/course/questionBanks/QuestionBank.tsx
+++ front-end/src/routes/course/questionBanks/QuestionBank.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import {Button, Col, Nav, Row, Tab} from 'react-bootstrap';
+import React, { useCallback, useEffect, useState } from 'react';
+import {Button, Col, Form, Modal, Nav, Row, Tab} from 'react-bootstrap';
 import {Link, useParams} from "react-router-dom";
 import TopNavbar from "../../../components/TopNavbar";
 import AppLayout from "../../../components/AppLayout";
 import questionDataType from "../../../components/questionTemplate/questionDataType";
 import Question from "../../../components/Question";
+import {useGlobalState} from "../../../components/GlobalStateProvider";
+import {getBackendApiUrl} from "../../../utils/url";
+import axios from 'axios';
+
+const AddTagModal = ({tags, addTag, show, onClose}: {tags: string[], addTag: any, show: boolean, onClose: () => void}) => {
+    const [value, setValue] = useState("");
+    const handleAdd = () => {
+        if (!tags.includes(value)) {
+            addTag((prevTags: string[]) => [...prevTags, value]);
+            onClose();
+        }
+    };
+    return (
+        <Modal show={show} onHide={onClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add New Tag</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group>
+                        <Form.Control type="text" placeholder="tag" autoFocus
+                            onChange={(event) => {setValue(event.target.value)}}/>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onClose}>Cancel</Button>
+                <Button variant="primary" onClick={handleAdd}>Add</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
 
 const QuestionsByTag = ({tag}: {tag: string}) => {
     let questionList: questionDataType[];
@@ -38,8 +70,24 @@ const QuestionsByTag = ({tag}: {tag: string}) => {
 
 function QuestionBank () {
     let params = useParams();
-    
-    const tags = ["Integer", "Float", "Cache", "Memory"];
+
+    const [tags, setTags] = useState<string[]>(["Integer", "Float", "Cache", "Memory"]);
+
+    const [addTagShow, setAddTagShow] = useState(false);
+
+    const {globalState} = useGlobalState();
+
+    const getTags = useCallback(async () => {
+        const url = getBackendApiUrl("/courses/" + params.course_name + "/tags");
+        const token = globalState.token;
+        const result = await axios.get(url, {headers: {Authorization: "Bearer " + token}});
+        console.log(result);
+        setTags(result.data.data);
+    }, [globalState.token, params.course_name]);
+
+    useEffect(() => {
+        getTags().catch();
+    }, [getTags]);
 
     return (
         <AppLayout>
@@ -58,7 +106,8 @@ function QuestionBank () {
                                 </Nav.Item>
                             ))}
                         </Nav>
-                        <Link to="#"><Button variant="primary">Add Tag</Button></Link>
+                        <Link to="#"><Button variant="primary" onClick={() => setAddTagShow(true)}>Add Tag</Button></Link>
+                        <AddTagModal tags={tags} addTag={setTags} show={addTagShow} onClose={() => setAddTagShow(false)}/>
                     </Col>
                     <Col sm={10}>
                         <Row className="text-end">
