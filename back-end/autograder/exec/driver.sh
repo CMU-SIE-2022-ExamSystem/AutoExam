a2147fc86e76b3e46efce15308ec492df6a7584a
HENG-ZH <hengzhan@andrew.cmu.edu>
2022-08-08 16:24:17 -0400

    Add functions for process modules in customized autograders.

--- back-end/autograder/exec/driver.sh
+++ back-end/autograder/exec/driver.sh
@@ -3,13 +3,16 @@
 # Usage: ./driver.sh
 
 # Run the autograder
+source env/bin/activate
 echo "<---Running--->"
-python3.9 main.py
+py=`python3.9 main.py $1`
 status=$?
 if [ ${status} -eq -1 ]; then
     echo "<---Failure--->"
-# else
-#     echo "<---Success--->"
+else
+    echo "<---Success--->"
 fi
+echo $py
 
 exit
+
