cb30a03a22573c5b112be27fb0e63556f060489e
Jingheng "Jimmy" Wang <jinghenw@andrew.cmu.edu>
2022-08-10 13:29:15 -0700

    Config: Add Form Modal (JW)

--- front-end/src/routes/course/config/ExamConfigQuestions.tsx
+++ front-end/src/routes/course/config/ExamConfigQuestions.tsx
@@ -4,7 +4,9 @@ import {useGlobalState} from "../../../components/GlobalStateProvider";
 import {ExamConfigSettingsType, useConfigStates} from "./ExamConfigStates";
 import {getBackendApiUrl} from "../../../utils/url";
 import axios from "axios";
-import {Button, Card, Form, Modal, Table} from "react-bootstrap";
+import {Button, Card, Form, Modal, Row, Table, Col} from "react-bootstrap";
+import questionDataType from "../../../components/questionTemplate/questionDataType";
+import Question from "../../../components/questionTemplate/readonly/QuestionReadOnly";
 
 interface tagProps {
     id: string;
@@ -55,53 +57,155 @@ const templateExamConfigSetting : ExamConfigSettingsType = {
     id: [], max_score: 0, scores: [], sub_question_number: 0, tag: "", title: ""
 };
 
-const AddModal = ({show, question, setQuestion, onSubmit, onCancel, tagList}: {show: boolean, question: ExamConfigSettingsType, setQuestion: React.Dispatch<React.SetStateAction<ExamConfigSettingsType>>, onSubmit: () => void, onCancel: () => void, tagList: tagProps[]}) => {
+const QuestionDisplayModal = ({show, toClose, question} : {show: boolean, toClose: () => void, question: questionDataType}) => {
+    return (
+        <Modal show={show} onHide={toClose} fullscreen>
+            <Modal.Header closeButton>
+                Question view
+            </Modal.Header>
+            <Modal.Body>
+                <Question questionData={question} questionId={1} />
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+const AddModal = ({show, question, setQuestion, onSubmit, onCancel, tagList, pickList}:
+                      {show: boolean, question: ExamConfigSettingsType, setQuestion: React.Dispatch<React.SetStateAction<ExamConfigSettingsType>>,
+                          onSubmit: () => void, onCancel: () => void, tagList: tagProps[], pickList: questionDataType[]}) => {
     const updateQuestion = (updateTerm: any) => {
-        const newQuestion = Object.assign({}, question, updateTerm);
+        let emptyArray = {};
+        if (updateTerm.sub_question_number) {
+            emptyArray = {scores: Array.from({length: updateTerm.sub_question_number}, _ => 0)}
+        }
+        const newQuestion = Object.assign({}, question, updateTerm, emptyArray);
         setQuestion(newQuestion);
         return newQuestion;
     }
+    const [badSubQuestionsNumber, setBadSubQuestionsNumber] = useState<boolean>(false);
+    const [displayQuestion, setDisplayQuestion] = useState<questionDataType | null>(null);
+    const [showDisplayModal, setShowDisplayModal] = useState<boolean>(false);
+    const filteredQuestions = ((arr: questionDataType[]) => {
+        if (question.sub_question_number > 0) {
+            return arr.filter(q => q.sub_question_number === question.sub_question_number);
+        } else {
+            return arr;
+        }
+    })(pickList);
+    const pickListChecks = filteredQuestions.map(filtered => {
+        const label = (
+            <span>{filtered.title} ({filtered.sub_question_number}) <i className="bi bi-zoom-in pointer-cursor" onClick={() => {
+                setDisplayQuestion(filtered);
+                setShowDisplayModal(true);
+            }}/></span>
+        )
+        return (
+            <Form.Check name="picked-questions"
+                        value={filtered.id}
+                        key={filtered.id}
+                        label={label}
+                        defaultChecked={question.id.includes(filtered.id)}
+                        onChange={(e) => {
+                            let prevIdList = question.id;
+                            let idx = question.id.findIndex(id => id === filtered.id);
+                            if (idx >= 0 && !e.target.checked) {
+                                prevIdList.splice(idx, 1);
+                                updateQuestion({id: prevIdList});
+                            }
+                            if (idx < 0 && e.target.checked) {
+                                prevIdList.push(filtered.id);
+                                if (question.sub_question_number === 0) {
+                                    console.log(updateQuestion({sub_question_number: filtered.sub_question_number, id: prevIdList}));
+                                } else {
+                                    updateQuestion({id: prevIdList});
+                                }
+                            }
+                        }}
+                        />
+        )
+    })
+    const availableValues = Array.from(new Set(pickList.map(item => item.sub_question_number)).values());
+
+    const pointsFormControls = Array.from({length: question.sub_question_number}, (value, index) => {
+        return (
+            <React.Fragment key={"SubquestionPoint_" + (index+1)}>
+                <Form.Label column xs={3}>Q{index + 1}</Form.Label>
+                <Col xs={3}><Form.Control type="number" min="0" step="0.01" value={question.scores[index]}
+                                          onChange={(e) => {
+                                              let newArray = question.scores;
+                                              newArray[index] = parseFloat(e.target.value) || 0;
+                                              const totalPoints = newArray.reduce((prev, next) => prev + next, 0);
+                                              updateQuestion({scores: newArray, max_score: totalPoints})
+                                          }}/></Col>
+            </React.Fragment>
+
+        )
+    })
+
     const validate = () => {
+        if (!availableValues.includes(question.sub_question_number)) {
+            setBadSubQuestionsNumber(true);
+            return;
+        }
         console.log(question);
     }
-    return (
-        <Modal show={show}>
-            <Modal.Header><Modal.Title>Add question</Modal.Title></Modal.Header>
-            <Modal.Body>
-                <Form>
-                    <Form.Group className="mb-2">
-                        <Form.Label>Tag</Form.Label>
-                        <Form.Select id="new-question-add-tag" value={question.tag} onChange={(e) => updateQuestion({tag: e.target.value})}>
-                            <option value={""}>Select a tag</option>
-                            {tagListOptions(tagList)}
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group className="mb-2">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control type="text"
-                                      className="mb-2"
-                                      required
-                                      id="new-question-add-title"
-                                      value={question.title}
-                                      onChange={(e) => updateQuestion({title: e.target.value})}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-2">
-                        <Form.Label>Generation method</Form.Label>
-                    </Form.Group>
-                    <Form.Group className="mb-2">
-                        <Form.Label>Number of Subquestions</Form.Label>
-                        <Form.Control type="number"
 
-                                      />
-                    </Form.Group>
+    let availableValuesText = (<small className={badSubQuestionsNumber ? "text-danger" : ""}>Following values are available: {availableValues.join(",")}.</small>);
+    return (
+        <>
+            <Modal show={show}>
+                <Modal.Header><Modal.Title>Add question</Modal.Title></Modal.Header>
+                <Form onSubmit={(e) => {e.preventDefault(); validate();}}>
+                    <Modal.Body>
+                        <Form.Group className="mb-2">
+                            <Form.Label>Tag</Form.Label>
+                            <Form.Select required id="new-question-add-tag" value={question.tag} onChange={(e) => updateQuestion({tag: e.target.value})}>
+                                <option value={""}>Select a tag</option>
+                                {tagListOptions(tagList)}
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Group className="mb-2">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text"
+                                          className="mb-2"
+                                          required
+                                          id="new-question-add-title"
+                                          value={question.title}
+                                          onChange={(e) => {updateQuestion({title: e.target.value})}}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-2">
+                            <Form.Label>Number of Subquestions</Form.Label>
+                            <Form.Control type="number" min="0" step="1" value={question.sub_question_number}
+                                          onChange={(e) => {setBadSubQuestionsNumber(false); updateQuestion({sub_question_number: parseInt(e.target.value) || 0})}}
+                                          isInvalid={badSubQuestionsNumber}
+                            />
+                            {question.tag && availableValuesText}
+                        </Form.Group>
+                        <Form.Group className="mb-2">
+                            <Form.Label>Appointed Questions</Form.Label>
+                            {pickListChecks}
+                            {pickListChecks.length === 0 && <div className="text-secondary fst-italic"><small>No available questions, please change your tag or number of subquestions.</small></div>}
+                        </Form.Group>
+                        {
+                            question.sub_question_number !== 0 &&
+                            <Form.Group className="mb-2" as={Row}>
+                                <Form.Label column xs={"12"}>Point Distributions</Form.Label>
+                                {pointsFormControls}
+                                <small>Total Points: {question.max_score}</small>
+                            </Form.Group>
+                        }
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={onCancel}>Back</Button>
+                        <Button variant="primary" type="submit">Submit</Button>
+                    </Modal.Footer>
                 </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={onCancel}>Back</Button>
-                <Button variant="primary" onClick={validate}>Submit</Button>
-            </Modal.Footer>
-        </Modal>
+            </Modal>
+            {
+                displayQuestion && <QuestionDisplayModal show={showDisplayModal} toClose={() => setShowDisplayModal(false)} question={displayQuestion} />
+            }
+        </>
     );
 }
 
@@ -192,6 +296,20 @@ const ExamConfigQuestions = () => {
         deleteFormCleanUp();
     }
 
+    const [queryQuestionList, setQueryQuestionList] = useState<questionDataType[]>([]);
+
+    const getQuestionList = useCallback(async () => {
+        if (questionInInterest.tag.length > 0) {
+            const url = getBackendApiUrl("/courses/" + courseName + "/questions?tag_id=" + questionInInterest.tag);
+            const token = globalState.token;
+            const result = await axios.get(url, {headers: {Authorization: "Bearer " + token}});
+            setQueryQuestionList(result.data.data);
+        }
+    }, [questionInInterest.tag])
+    useEffect(() => {
+        getQuestionList().catch();
+    }, [questionInInterest.tag])
+
     const settingsToQuestion = examConfigState?.settings?.map((setting, index) => settingToQuestion(setting, index, tags, editQuestionWrapper, deleteQuestionWrapper));
     return (
         <div>
@@ -200,8 +318,8 @@ const ExamConfigQuestions = () => {
                 {(settingLength >= 2) && (<Button variant="warning"><i className="bi bi-list me-1"/>Change Order</Button>)}
             </div>
             {settingsToQuestion}
-            <AddModal show={addQuestionModalShow} question={questionInInterest} setQuestion={setQuestionInInterest} onSubmit={addFormHandler} onCancel={addFormCleanUp} tagList={tags}/>
-            <AddModal show={editQuestionModalShow} question={questionInInterest} setQuestion={setQuestionInInterest} onSubmit={editFormHandler} onCancel={editFormCleanUp} tagList={tags}/>
+            <AddModal show={addQuestionModalShow} question={questionInInterest} setQuestion={setQuestionInInterest} onSubmit={addFormHandler} onCancel={addFormCleanUp} tagList={tags} pickList={queryQuestionList}/>
+            <AddModal show={editQuestionModalShow} question={questionInInterest} setQuestion={setQuestionInInterest} onSubmit={editFormHandler} onCancel={editFormCleanUp} tagList={tags} pickList={queryQuestionList}/>
             <DeleteModal show={deleteQuestionModalShow} oldQuestion={questionInInterest} onSubmit={deleteFormHandler} onCancel={deleteFormCleanUp} />
         </div>
     )
